d56b6c53667d98eed7338078bb69270217d0cb70
234875135 <swagging123@proton.me>
2026-05-21 20:42:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const siteConfig = {
       label: "Current release",
       status: "Ready",
       hash: "version-9377ee10133e4be3",
-      file: "/boostrap/latest.exe",
+      file: "/downloads/version-9377ee10133e4be3",
       note: "For the live Roblox client."
     },
     {
